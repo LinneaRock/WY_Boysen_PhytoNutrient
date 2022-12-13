9ec109472a3d08d67bd3e9e6857aba55beed7458
Linnea Ann Rock <lrock1@uwyo.edu>
2022-12-12 17:02:42 -0700

    fixed code to call USGS data

--- Data/data_call_USGS.R
+++ Data/data_call_USGS.R
@@ -12,12 +12,23 @@ windrvroutletparams <- whatNWISdata(siteNumber = '06259000')
 sitedata <- readNWISsite(siteNumbers = c("06258000", "06253000", "06236100", '06259000'))
                          
 # Load parameters we want ####
-alldat <- readNWISqw(siteNumbers = c("06258000", "06253000", "06236100", '06259000'),
-                          parameterCd = c('00010', '00060', '000095', '00400', '00600', '00605', '00608', '00631', '00665','00671'),
-                          startDate = '2002-01-01') #|>
+discharge <- readNWISdata(siteNumbers = c("06258000", "06253000", "06236100", '06259000'),
+                           parameterCd = '00060',
+                           startDate = '2002-01-01')
+# chemdat <- readNWISqw(siteNumbers = c("06258000", "06253000", "06236100", '06259000'),
+#                           parameterCd = c('00600', '00605', '00608', '00631', '00665','00671'),
+#                           startDate = '2002-01-01') #|> readNWISqw will be removed. Need to use the code below to get water qual data from wqp:
+#https://cran.r-project.org/web/packages/dataRetrieval/vignettes/qwdata_changes.html
   # mutate(parameter = case_when(param_cd == '00600' ~ 'TN_mgL', 
   #                         param_cd == '00605' ~ 'TON_mgL', 
   #                         param_cd == '00613' ~ 'Wind River Inlet', 
   #                         param_cd == '00631' ~ 'Wind River Outlet',
   #                         param_cd == '00665',
   #                         param_cd == '00671'))
+
+
+site_ids <- c("06258000", "06253000", "06236100", '06259000')
+parameterCd <- c('00010', '00095', '00400','00600', '00605', '00608', '00631', '00665','00671')
+
+chemdata <- readWQPqw(paste0("USGS-", site_ids), parameterCd,
+                     startDate = '2002-01-01')
