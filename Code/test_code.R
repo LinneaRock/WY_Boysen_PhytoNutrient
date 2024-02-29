87d5e3f0ff1a33882c8318328389914e37b84b3e
LinneaRock <lrock@wisc.edu>
2024-02-29 15:45:05 -0700

    going over some nmds with jordy

--- Code/test_code.R
+++ Code/test_code.R
@@ -76,9 +76,92 @@ dist_data <- as.matrix(dist_data)
 dist_data <- replace(dist_data, is.na(dist_data), 0)
 
 dist <- vegdist(dist_data, method = 'bray')
+
+dist
+
+adonis2()
+
+# ordiplot()
+
+# NMDS
 nmds <- metaMDS(dist)
 
 scores(nmds) |>
   as_tibble(rownames='Group') |>
   ggplot(aes(x=NMDS1, y=NMDS2)) +
   geom_point()
+
+# i can use lines to connect my points in nmds by site and time
+ggsave('Figures/test.png')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+data(dune)
+data(dune.env)
+## default test by terms
+adonis2(dune ~ Management*A1, data = dune.env)
+## overall tests
+adonis2(dune ~ Management*A1, data = dune.env, by = NULL)
+
+### Example of use with strata, for nested (e.g., block) designs.
+dat <- expand.grid(rep=gl(2,1), NO3=factor(c(0,10)),field=gl(3,1) )
+dat
+Agropyron <- with(dat, as.numeric(field) + as.numeric(NO3)+2) +rnorm(12)/2
+Schizachyrium <- with(dat, as.numeric(field) - as.numeric(NO3)+2) +rnorm(12)/2
+total <- Agropyron + Schizachyrium
+total
+dotplot(total ~ NO3, dat, jitter.x=TRUE, groups=field,
+        type=c('p','a'), xlab="NO3", auto.key=list(columns=3, lines=TRUE) )
+
+Y <- data.frame(Agropyron, Schizachyrium)
+mod <- metaMDS(Y, trace = FALSE)
+plot(mod)
+### Ellipsoid hulls show treatment
+with(dat, ordiellipse(mod, field, kind = "ehull", label = TRUE))
+### Spider shows fields
+with(dat, ordispider(mod, field, lty=3, col="red"))
+
+### Incorrect (no strata)
+adonis2(Y ~ NO3, data = dat, permutations = 199)
+## Correct with strata
+with(dat, adonis2(Y ~ NO3, data = dat, permutations = 199, strata = field))
+
