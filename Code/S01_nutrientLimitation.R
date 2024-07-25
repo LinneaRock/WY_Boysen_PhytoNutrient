0792298aa43cb44eae203434a15fb07019b0e8b3
Linnea Ann Rock <lrock1@uwyo.edu>
2024-07-25 15:07:03 -0600

    nutrient limitation figure

--- Code/S01_nutrientLimitation.R
+++ Code/S01_nutrientLimitation.R
@@ -7,27 +7,8 @@ source('Data/CALL_DATA_LIB.R')
 lim_dat <- BoysenNutrient |>
   select(-ChemUnits, -julianday, -SampleDepth) |>
   pivot_wider(names_from = ShortName_Revised, values_from = ChemValue) |>
-  mutate(DIN.TP = ((NO3+NH4)/TP) * 2.11306) # multiply by constant to make molar ratio
-
-
-# DIN:TP limitation ####
-# maybe not viable metric becuase of DIN detection limits though
-ggplot() +
-  geom_jitter(lim_dat, mapping = aes(log10(TP), log10(DIN.TP), 
-                                      color = WaterbodyName), size=2) +
-  geom_abline(slope = 0, intercept = log10(3.4), linetype = "dashed") + # bergstrom P limitation line
-  geom_abline(slope = 0, intercept = log10(1.5), linetype = "dashed") +  # bergstrom N limitation line
-  theme_minimal() +
-  labs(y = "Log DIN:TP", x = "Log TP") +
-  annotate('text', label = 'Predicted N limitation below dashed line \n (Bergström, 2010)', 
-           x = -2.5, y = 0.17, hjust = 0, size = 4) +
-  annotate('text', label = 'Predicted P limitation above dashed line \n (Bergström, 2010)', 
-           x = -2.5, y = 0.55, hjust = 0, size = 4) +
-  scale_color_viridis_d('',option='turbo')
-
-
-
-
+  mutate(DIN.TP = ((NO3+NH4)/TP) * 2.11306)  |> # multiply by constant to make molar ratio
+  mutate(WaterbodyName=factor(WaterbodyName, levels=c('Lacustrine Pelagic: Dam', 'East Shore','Cottonwood Creek Bay','Tough Creek Campground','Transitional Pelagic: Sand Mesa','Riverine Pelagic: Freemont 1','Fremont Bay'))) 
 
 
 # TN:TP limitation ####
@@ -68,7 +49,7 @@ ggsave('Figures/nutrient_limitation.png', height=5,width=7,units='in',dpi=1200)
 
 
 
-# TN:TP limitation ####
+# TN:TP limitation by month ####
 ggplot() +
   geom_jitter(lim_dat, mapping = aes(TP, TN.TP, 
                                      color = month), size=2) +
@@ -94,3 +75,26 @@ ggplot() +
   # annotate('text', label = 'Predicted P limitation \n (Ptacnik et al., 2010)',
   #          x = 0.55, y = 2.05, hjust = 0, size = 2.5, color = "#ffc857") +
   # scale_color_viridis_d('',option='turbo') 
+
+
+
+
+
+
+
+
+# DIN:TP limitation ####
+# maybe not viable metric because of DIN detection limits
+ggplot() +
+  geom_jitter(lim_dat, mapping = aes(log10(TP), log10(DIN.TP), 
+                                     color = WaterbodyName), size=2) +
+  geom_abline(slope = 0, intercept = log10(3.4), linetype = "dashed") + # bergstrom P limitation line
+  geom_abline(slope = 0, intercept = log10(1.5), linetype = "dashed") +  # bergstrom N limitation line
+  theme_minimal() +
+  labs(y = "Log DIN:TP", x = "Log TP") +
+  annotate('text', label = 'Predicted N limitation below dashed line \n (Bergström, 2010)', 
+           x = -2.5, y = 0.17, hjust = 0, size = 4) +
+  annotate('text', label = 'Predicted P limitation above dashed line \n (Bergström, 2010)', 
+           x = -2.5, y = 0.55, hjust = 0, size = 4) +
+  scale_color_viridis_d('',option='turbo')
+
